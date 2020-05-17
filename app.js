555cf0281b15fdd67035f3c10cc7eb3370cf8562
Juji <jujiju@Jujis-MBP.station>
2020-05-17 21:35:57 +0200

    index page almost complete

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function bgChanger() {
-  if (this.scrollY > this.innerHeight / 1.5) {
+  if (this.scrollY > this.innerHeight / 0.5) {
     document.body.classList.add("bg-active");
   } else {
     document.body.classList.remove("bg-active");
